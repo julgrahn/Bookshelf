fb53918885245c4b6912c65000fe692abfa3ba5a
julgrahn <alexander.grahn@hotmail.se>
2020-05-25 04:57:48 +0200

    first commit

---
+++ Book.cs
@@ -0,0 +1,24 @@
+﻿namespace Bookshelf
+{
+    public class Book
+    {
+        public string Title;
+        public string Author;
+        public string Type;
+    }
+
+    public class Novel : Book
+    {
+
+    }
+
+    public class Journal : Book
+    {
+
+    }
+
+    public class Poetry : Book
+    {
+
+    }
+}
